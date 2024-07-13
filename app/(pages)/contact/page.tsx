bdc5567f920077a4eac35a33da9872e532e548a8
ritik-apurwa <ritikapurwa3500@gmail.com>
2024-07-13 15:58:37 +0530

    dsd

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -5,8 +5,8 @@ import { FaYoutube } from "react-icons/fa";
 
 const page = () => {
   return (
-    <section className="mx-auto bg-blue-100 min-h-screen">
-      <div className="flex flex-col gap-y-3 pt-4">
+    <section className=" bg-blue-100 min-h-screen">
+      <div className="flex max-w-7xl mx-auto flex-col gap-y-3 pt-4">
         <div className="flex flex-row gap-x-2 p-2">
           <Phone />
           +915665566565
